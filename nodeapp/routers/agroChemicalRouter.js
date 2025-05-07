dad8fd23d62012c357ec34aff1d1ff4d0ea6e581
AnshuGoud <acchagownianshu@gmail.com>
2025-05-07 07:30:29 +0000

    updated-agrochemical-router

--- nodeapp/routers/agroChemicalRouter.js
+++ nodeapp/routers/agroChemicalRouter.js
@@ -6,20 +6,14 @@ const {
     updateAgroChemical,
     deleteAgroChemical
 } = require('../controllers/agroChemicalController');
-// const { validateToken } = require('../authUtils');
+const { validateToken } = require('../authUtils');
 
 const router = express.Router();
 
-// router.get('/getAllAgroChemicals', validateToken, getAllAgroChemicals);
-// router.get('/getAgroChemicalById/:id', validateToken, getAgroChemicalById);
-// router.post('/addAgroChemical', validateToken, addAgroChemical);
-// router.put('/updateAgroChemical/:id', validateToken, updateAgroChemical);
-// router.delete('/deleteAgroChemical/:id', validateToken, deleteAgroChemical);
-
-router.get('/getAllAgroChemicals', getAllAgroChemicals);
-router.get('/getAgroChemicalById/:id', getAgroChemicalById);
-router.post('/addAgroChemical', addAgroChemical);
-router.put('/updateAgroChemical/:id', updateAgroChemical);
-router.delete('/deleteAgroChemical/:id', deleteAgroChemical);
+router.get('/getAllAgroChemicals', validateToken, getAllAgroChemicals);
+router.get('/getAgroChemicalById/:id', validateToken, getAgroChemicalById);
+router.post('/addAgroChemical', validateToken, addAgroChemical);
+router.put('/updateAgroChemical/:id', validateToken, updateAgroChemical);
+router.delete('/deleteAgroChemical/:id', validateToken, deleteAgroChemical);
 
 module.exports = router;
