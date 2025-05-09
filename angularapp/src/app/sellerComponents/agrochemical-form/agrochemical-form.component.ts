3b3273775cddc9b0eda123a5a62c82d144fa37bd
AnshuGoud <acchagownianshu@gmail.com>
2025-05-09 05:31:02 +0000

    UPdating code

--- angularapp/src/app/sellerComponents/agrochemical-form/agrochemical-form.component.ts
+++ angularapp/src/app/sellerComponents/agrochemical-form/agrochemical-form.component.ts
@@ -1,3 +1,5 @@
+
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -8,82 +10,47 @@ import { AgrochemicalService } from 'src/app/services/agrochemical.service';
   templateUrl: './agrochemical-form.component.html',
 })
 export class AgrochemicalFormComponent implements OnInit {
-  agroForm!: FormGroup;
-  isEditMode = false;
-  imagePreview: string = '';
-  imageRequired = false;
-  agroId: string | null = null;
-  showSuccessModal = false;
+  
+  agroForm: FormGroup;
+  imageFile: File | null = null;
 
-  constructor(
-    private fb: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private agroService: AgrochemicalService
-  ) {}
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private agroChemicalService:AgrochemicalService,private router:Router) {
     this.agroForm = this.fb.group({
-      name: ['', Validators.required],
-      brand: ['', Validators.required],
-      category: ['', Validators.required],
-      description: ['', Validators.required],
-      unit: ['', Validators.required],
-      price: ['', [Validators.required, Validators.min(1)]],
-      imageUrl: ['']
+      name: [''],
+      brand: [''],
+      category: [''],
+      description: [''],
+      unit: [''],
+      price: [''],
+      image: [null]
     });
-
-    this.agroId = this.route.snapshot.paramMap.get('id');
-    if (this.agroId) {
-      this.isEditMode = true;
-      this.agroService.getAgrochemicalById(this.agroId).subscribe(data => {
-        this.agroForm.patchValue(data);
-        this.imagePreview = data.imageUrl;
-      });
-    }
+  }
+  ngOnInit(): void {
+    
   }
 
-  onFileChange(event: any): void {
-    const file = event.target.files[0];
-    if (file && ['image/jpeg', 'image/png', 'image/jpg'].includes(file.type)) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.imagePreview = reader.result as string;
-        this.agroForm.patchValue({ imageUrl: this.imagePreview });
-        this.imageRequired = false;
-      };
-      reader.readAsDataURL(file);
-    } else {
-      alert('Only JPG, JPEG, PNG files are allowed.');
-    }
+  onFileChange(event: any) {
+    this.imageFile = event.target.files[0];
   }
 
-  onSubmit(): void {
-    if (this.agroForm.invalid || !this.imagePreview) {
-      this.imageRequired = !this.imagePreview;
-      this.agroForm.markAllAsTouched();
-      return;
+  submitForm() {
+    const formData = new FormData();
+
+  formData.append('name', this.agroForm.get('name')?.value);
+  formData.append('brand', this.agroForm.get('brand')?.value);
+  formData.append('category', this.agroForm.get('category')?.value);
+  formData.append('description', this.agroForm.get('description')?.value);
+  formData.append('unit', this.agroForm.get('unit')?.value);
+  formData.append('price', this.agroForm.get('price')?.value);
+
+
+    if (this.imageFile) {
+      formData.append('image', this.imageFile);
     }
 
-    const data = { ...this.agroForm.value, imageUrl: this.imagePreview };
-
-    if (this.isEditMode && this.agroId) {
-      this.agroService.updateAgrochemical(this.agroId, data).subscribe(() => {
-        this.showSuccessModal = true;
-      });
-    } else {
-      this.agroService.addAgrochemical(data).subscribe(() => {
-        this.showSuccessModal = true;
-      });
-    }
-  }
-
-  navigateToView(): void {
-    this.showSuccessModal = false;
-    this.router.navigate(['/seller/view-agrochemical']);
-  }
-
-  goBack(): void {
-    this.router.navigate(['/seller/view-agrochemical']);
+    this.agroChemicalService.addAgrochemical(formData).subscribe(() => {
+      this.router.navigate(['/seller/view-agrochemical'])
+    })
+    
   }
 }
