88a27b847c7d2f783f2c60d7a9ab6b35949c7fb5
SaiTeja134 <saiteja.gangavaram09@gmail.com>
2025-05-11 18:03:11 +0000

    added-sync-code

---
+++ nodeapp/index.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const logger = require('./loggers/logger');
+const userRoutes = require('./routers/userRouter');
+const cropRoutes = require('./routers/cropRouter');
+const agroChemicalRoutes = require('./routers/agroChemicalRouter');
+const requestRoutes = require('./routers/requestRouter');
+
+const app = express();
+
+
+app.set('trust proxy', 1);
+
+// Rate Limiter: Limits each IP to 100 requests per 15 minutes
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: { error: 'Too many requests, please try again later.' },
+    headers: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
+
+app.use('/user', userRoutes);
+app.use('/crop', cropRoutes);
+app.use('/agroChemical', agroChemicalRoutes);
+app.use('/request', requestRoutes);
+
+mongoose.set('strictQuery', true).connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        logger.info('Connected to MongoDB');
+        app.listen(8080, () => {
+            logger.info(`Server is running on port: 8080`);
+        });
+    })
+    .catch(err => {
+        logger.error(`Failed to connect to MongoDB: ${err.message}`);
+    });
+
+app.use((err, _req, res, _next) => {
+    logger.error(`Error: ${err.stack}`);
+    res.status(err.status || 500).json({
+        message: err.message || 'Internal Server Error'
+    });
+});
--- nodeapp/routers/cropRouter.js
+++ nodeapp/routers/cropRouter.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const {
+    getAllCrops,
+    getCropById,
+    getCropsByUserId,
+    addCrop,
+    updateCrop,
+    deleteCrop
+} = require('../controllers/cropController');
+const { validateToken } = require('../authUtils');
 
+const router = express.Router();
+
+router.get('/getAllCrops', getAllCrops);
+router.get('/getCropById/:id', getCropById);
+router.get('/user/getCropByUserId/:userId', getCropsByUserId);
+router.post('/addCrop', addCrop);
+router.put('/updateCrop/:id', updateCrop);
+router.delete('/deleteCrop/:id', deleteCrop);
+module.exports = router;
