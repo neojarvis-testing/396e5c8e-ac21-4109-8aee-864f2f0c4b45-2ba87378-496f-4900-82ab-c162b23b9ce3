eafe6972e7b2a518fecb60556575b070cb059b85
peerhammad007 <Peerhammad007@gmail.com>
2025-05-11 11:52:53 +0000

    fetching from main

--- nodeapp/routers/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getUserByEmailAndPassword, addUser } = require('../controllers/userController');
-
-const router = express.Router();
-
-router.post('/login', getUserByEmailAndPassword);
-router.post('/signup', addUser);
-
-module.exports = router;
